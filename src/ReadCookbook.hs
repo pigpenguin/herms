01cc5daa1ac09d272b9ed33cd4a66ac8407201d5
Ethan Rooke <ethan@rooke.org>
2018-06-22 23:20:07 -0700

    Cleaned up imports, added more helpful comments

--- src/ReadCookbook.hs
+++ src/ReadCookbook.hs
@@ -4,29 +4,58 @@ module ReadCookbook (parseCookbook, showJson) where
 import           Control.Applicative ((<|>), liftA2)
 import           Data.Aeson ((.:),(.=),ToJSON(..),FromJSON(..))
 import qualified Data.Aeson as Aeson (decode, encode, object, withObject)
-import qualified Data.Aeson.Encode.Pretty as Aeson (encodePretty)
+import qualified Data.Aeson.Encode.Pretty as Aeson
+  ( Config(..)
+  , Indent(Spaces)
+  , NumberFormat(Generic)
+  , encodePretty'
+  , keyOrder
+  )
 import           Data.ByteString.Lazy (ByteString, unpack)
 import           Data.Char (chr)
 import           Text.Read (readMaybe)
 
 import Types (Ingredient(..), Recipe(..), RecipeBook)
 
--- This definition might be a tad convoluted
+-- Attempts to parse a textual representation of a cookbook. Returns the first
+-- succesful attempt between reading in the old school format and a JSON format
 parseCookbook :: ByteString -> Maybe RecipeBook
 parseCookbook = readNative <||> readJson
   where
     (<||>) = liftA2 (<|>)
 
+
+-- Attempts to parse a newline seperated list of shown recipes.
 readNative :: ByteString -> Maybe RecipeBook
 readNative = mapM readMaybe . lines . toString
   where
     toString = map (chr . fromIntegral) . unpack
 
+-- Attempts to parse a JSON list of JSON encoded recipes
 readJson :: ByteString -> Maybe RecipeBook
 readJson = Aeson.decode
 
+-- This dictates the order in which the pretty printer will display keys
+keys = [ "recipeName"
+       , "description"
+       , "servingSize"
+       , "ingredients"
+       , "directions"
+       , "tags"
+       , "ingredientName"
+       , "quantity"
+       , "unit"
+       , "attribute"
+       ]
+
+displayConfig :: Aeson.Config
+displayConfig = Aeson.Config { Aeson.confIndent = Aeson.Spaces 2
+                             , Aeson.confCompare = Aeson.keyOrder keys
+                             , Aeson.confNumFormat = Aeson.Generic
+                             , Aeson.confTrailingNewline = True}
+
 showJson :: RecipeBook -> ByteString
-showJson = Aeson.encodePretty
+showJson = Aeson.encodePretty' displayConfig
 
 -- JSON parsing instances for Ingredient
 
@@ -38,11 +67,10 @@ instance FromJSON Ingredient where
                <*> o .: "attribute"
 
 instance ToJSON Ingredient where
-  toJSON i = Aeson.object [
-                          "quantity"       .= quantity i,
-                          "unit"           .= unit i,
-                          "ingredientName" .= ingredientName i,
-                          "attribute"      .= attribute i
+  toJSON i = Aeson.object [ "quantity"       .= quantity i
+                          , "unit"           .= unit i
+                          , "ingredientName" .= ingredientName i
+                          , "attribute"      .= attribute i
                           ]
 
 -- JSON parsing instances for Recipe
@@ -57,11 +85,10 @@ instance FromJSON Recipe where
            <*> o .: "tags"
 
 instance ToJSON Recipe where
-  toJSON r = Aeson.object [
-                          "recipeName"  .= recipeName r,
-                          "description" .= description r,
-                          "servingSize" .= servingSize r,
-                          "ingredients" .= ingredients r,
-                          "directions"  .= directions r,
-                          "tags"        .= tags r
+  toJSON r = Aeson.object [ "recipeName"  .= recipeName r
+                          , "description" .= description r
+                          , "servingSize" .= servingSize r
+                          , "ingredients" .= ingredients r
+                          , "directions"  .= directions r
+                          , "tags"        .= tags r
                           ]
